a1410c41e83785a05f87a9078dbea0e62e2eef5f
Abhishek Kumar <153101074+Abhishek29-hash@users.noreply.github.com>
2026-02-13 00:32:45 +0530

    Create delete_tail_DLL.cpp

---
+++ delete_tail_DLL.cpp
@@ -0,0 +1,67 @@
+#include<bits/stdc++.h>
+using namespace std;
+struct Node{
+    public:
+    int data;
+    Node* next;
+    Node* back;
+
+    Node(int data,Node* next, Node* back){
+        this->data = data;
+        this->next = next;
+        this->back = back;
+    }
+
+    Node(int data){
+        this->data = data;
+        next = nullptr;
+        back = nullptr;
+    }
+};
+
+Node* convertarr2DLL(vector<int>&v){
+    Node* head = new Node(v[0]);
+    Node* prev = head;
+    for(int i = 1;i<v.size();i++){
+        Node* temp = new Node(v[i],nullptr,prev);
+        prev->next = temp;
+        prev = temp;
+    }
+    return head;
+}
+
+Node* delete_tail(Node* head){
+    if(head == nullptr || head->next == nullptr){
+        return NULL;
+    }
+     Node* tail = head;
+    while(tail->next != nullptr){
+        tail = tail->next;
+    }
+   
+    Node* newtail = tail->back;
+    newtail->next = nullptr;
+    tail->back =nullptr;
+    delete tail;
+    return head;
+}
+
+void print(Node* head){
+    while(head){
+        cout << head->data <<" ";
+        head = head->next;
+    }
+    cout <<"\n->>>>>>>>>>>>>>>>>>\n";
+}
+
+int main(){
+    vector<int>v = {1,2,3,4,5,6,7};
+    Node* head = convertarr2DLL(v);
+    Node* temp = head;
+
+    print(head);
+
+    head = delete_tail(head);
+
+    print(head);
+}
